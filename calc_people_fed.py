0a3e71c62cbac424a3dbd8d425b684e73eec89ec
becky <rchaplinkramer@gmail.com>
2022-04-25 16:25:35 -0700

    actually it's calc_people_fed.py not pollination_pipeline_calc_ppl_fed.py

--- calc_people_fed.py
+++ calc_people_fed.py
@@ -1,4 +1,5 @@
-"""These calculations are for the CI global restoration project."""
+"""These calculations are for the connecting make_poll_suff to realized_pollination."""
+""" Was pollination_pipeline.py and then pollination_pipeline_calc_ppl_fed.py"""
 #conda activate py38_gdal312
 #conda activate py39_ecoprocess
 
@@ -46,36 +47,94 @@ def main():
     #python make_poll_suff.py ./Sc2v5_md5_a3ce41871b255adcd6e1c65abfb1ddd0.tif
     #python make_poll_suff.py ./Sc2v6_md5_dc75e27f0cb49a84e082a7467bd11214.tif
     #python make_poll_suff.py "D:\ecoshard\CI_PPC\scenarios\Sc3v1_PNVnoag_md5_c07865b995f9ab2236b8df0378f9206f.tif"
+    #python make_poll_suff.py "D:\repositories\tnc-sci-ncscobenefits\workspace\data\marine_ESACCI-LC-L4-LCCS-Map-300m-P1Y-2020-v2.1.1_md5_e6a8da.tif"
+    #python make_poll_suff.py "D:\repositories\tnc-sci-ncscobenefits\workspace\data\marine_ESACCI-LC-L4-LCCS-Map-300m-P1Y-1992-v2.0.7cds_compressed_md5_83ec1b.tif"
 
 
-    calculation_list = [
-        #{
-        #   'expression': 'raster1*raster2*raster3*(raster4>0)+(raster4<1)*-9999',
-        #   'symbol_to_path_map': {
-        #       'raster1': "monfreda_2008_yield_poll_dep_ppl_fed_5min.tif", #https://storage.googleapis.com/critical-natural-capital-ecoshards/monfreda_2008_yield_poll_dep_ppl_fed_5min.tif
-        #       'raster2': r"workspace_poll_suff\churn\poll_suff_hab_ag_coverage_rasters\poll_suff_ag_coverage_prop_10s_ESACCI-LC-L4-LCCS-Map-300m-P1Y-2020_modVCFTree1km_md5_1cef3d5ad126b8bb34deb19d9ffc7d46.tif",
-        #       'raster3': "esa_pixel_area_ha_md5_1dd3298a7c4d25c891a11e01868b5db6.tif", # https://storage.googleapis.com/ecoshard-root/esa_pixel_area_ha_md5_1dd3298a7c4d25c891a11e01868b5db6.tif
-        #       'raster4': r"workspace_poll_suff\churn\ag_mask\ESACCI-LC-L4-LCCS-Map-300m-P1Y-2020_modVCFTree1km_md5_1cef3d5ad126b8bb34deb19d9ffc7d46_ag_mask.tif"
-        #    },
-        #    'target_nodata': -9999,
-        #    'target_pixel_size': (0.0027777777777777778,-0.0027777777777777778),
-        #    'resample_method': 'near',
-        #    'target_raster_path': "pollination_ppl_fed_on_ag_10s_esa2020mVCF.tif",
-        #},
+    calculation_list = [  # For TNC NbS project
         {
            'expression': 'raster1*raster2*raster3*(raster4>0)+(raster4<1)*-9999',
-           'symbol_to_path_map': {
+           'symbol_to_path_map': { 
                'raster1': "monfreda_2008_yield_poll_dep_ppl_fed_5min.tif", #https://storage.googleapis.com/critical-natural-capital-ecoshards/monfreda_2008_yield_poll_dep_ppl_fed_5min.tif
-               'raster2': r"D:\repositories\pollination_sufficiency\workspace_poll_suff\churn\poll_suff_hab_ag_coverage_rasters\poll_suff_ag_coverage_prop_10s_Sc3v1_PNVnoag_md5_c07865b995f9ab2236b8df0378f9206f.tif",
+               'raster2': r"workspace_poll_suff\churn\poll_suff_hab_ag_coverage_rasters\poll_suff_ag_coverage_prop_10s_marine_ESACCI-LC-L4-LCCS-Map-300m-P1Y-1992-v2.0.7cds_compressed_md5_83ec1b.tif",
                'raster3': "esa_pixel_area_ha_md5_1dd3298a7c4d25c891a11e01868b5db6.tif", # https://storage.googleapis.com/ecoshard-root/esa_pixel_area_ha_md5_1dd3298a7c4d25c891a11e01868b5db6.tif
-               'raster4': r"D:\repositories\pollination_sufficiency\workspace_poll_suff\churn\ag_mask\Sc3v1_PNVnoag_md5_c07865b995f9ab2236b8df0378f9206f_ag_mask.tif",
+               'raster4': r"workspace_poll_suff\churn\ag_mask\marine_ESACCI-LC-L4-LCCS-Map-300m-P1Y-1992-v2.0.7cds_compressed_md5_83ec1b_ag_mask.tif"
             },
             'target_nodata': -9999,
             'target_pixel_size': (0.0027777777777777778,-0.0027777777777777778),
             'resample_method': 'near',
-            'target_raster_path': "pollination_ppl_fed_on_ag_10s_Sc3v1_PNVnoag.tif",
+            'target_raster_path': "pollination_ppl_fed_on_ag_10s_esa2020mar.tif",
+        },
+        {
+           'expression': 'raster1*raster2*raster3*(raster4>0)+(raster4<1)*-9999',
+           'symbol_to_path_map': {
+               'raster1': "monfreda_2008_yield_poll_dep_ppl_fed_5min.tif", #https://storage.googleapis.com/critical-natural-capital-ecoshards/monfreda_2008_yield_poll_dep_ppl_fed_5min.tif
+               'raster2': r"D:\repositories\pollination_sufficiency\workspace_poll_suff\churn\poll_suff_hab_ag_coverage_rasters\poll_suff_ag_coverage_prop_10s_marine_ESACCI-LC-L4-LCCS-Map-300m-P1Y-2020-v2.1.1_md5_e6a8da.tif",
+               'raster3': "esa_pixel_area_ha_md5_1dd3298a7c4d25c891a11e01868b5db6.tif", # https://storage.googleapis.com/ecoshard-root/esa_pixel_area_ha_md5_1dd3298a7c4d25c891a11e01868b5db6.tif
+               'raster4': r"D:\repositories\pollination_sufficiency\workspace_poll_suff\churn\ag_mask\marine_ESACCI-LC-L4-LCCS-Map-300m-P1Y-2020-v2.1.1_md5_e6a8da_ag_mask.tif",
+            },
+            'target_nodata': -9999,
+            'target_pixel_size': (0.0027777777777777778,-0.0027777777777777778),
+            'resample_method': 'near',
+            'target_raster_path': "pollination_ppl_fed_on_ag_10s_esa1992mar.tif",
         },
     ]
+
+
+    #calculation_list = [  #For CI PPC project
+    #    {
+    #       'expression': 'raster1*raster2*raster3*(raster4>0)+(raster4<1)*-9999',
+    #       'symbol_to_path_map': { 
+    #           'raster1': "monfreda_2008_yield_poll_dep_ppl_fed_5min.tif", #https://storage.googleapis.com/critical-natural-capital-ecoshards/monfreda_2008_yield_poll_dep_ppl_fed_5min.tif
+    #           'raster2': r"workspace_poll_suff\churn\poll_suff_hab_ag_coverage_rasters\poll_suff_ag_coverage_prop_10s_Sc1v3_clip_compressed_md5_182b5f085cbec0dc976135f00f810b7c.tif",
+    #           'raster3': "esa_pixel_area_ha_md5_1dd3298a7c4d25c891a11e01868b5db6.tif", # https://storage.googleapis.com/ecoshard-root/esa_pixel_area_ha_md5_1dd3298a7c4d25c891a11e01868b5db6.tif
+    #           'raster4': r"workspace_poll_suff\churn\ag_mask\Sc1v3_clip_compressed_md5_182b5f085cbec0dc976135f00f810b7c_ag_mask.tif"
+    #        },
+    #        'target_nodata': -9999,
+    #        'target_pixel_size': (0.0027777777777777778,-0.0027777777777777778),
+    #        'resample_method': 'near',
+    #        'target_raster_path': "pollination_ppl_fed_on_ag_10s_Sc1v3_clip.tif",
+    #    },
+    #    {
+    #       'expression': 'raster1*raster2*raster3*(raster4>0)+(raster4<1)*-9999',
+    #       'symbol_to_path_map': { 
+    #           'raster1': "monfreda_2008_yield_poll_dep_ppl_fed_5min.tif", #https://storage.googleapis.com/critical-natural-capital-ecoshards/monfreda_2008_yield_poll_dep_ppl_fed_5min.tif
+    #           'raster2': r"workspace_poll_suff\churn\poll_suff_hab_ag_coverage_rasters\poll_suff_ag_coverage_prop_10s_Sc2v3_Griscom_CookPatton2050_smithpnv_md5_82c2f863d49f5a25c0b857865bfdb4b0.tif",
+    #           'raster3': "esa_pixel_area_ha_md5_1dd3298a7c4d25c891a11e01868b5db6.tif", # https://storage.googleapis.com/ecoshard-root/esa_pixel_area_ha_md5_1dd3298a7c4d25c891a11e01868b5db6.tif
+    #           'raster4': r"workspace_poll_suff\churn\ag_mask\Sc2v3_Griscom_CookPatton2050_smithpnv_md5_82c2f863d49f5a25c0b857865bfdb4b0_ag_mask.tif"
+    #        },
+    #        'target_nodata': -9999,
+    #        'target_pixel_size': (0.0027777777777777778,-0.0027777777777777778),
+    #        'resample_method': 'near',
+    #        'target_raster_path': "pollination_ppl_fed_on_ag_10s_Sc2v3_Griscom2050.tif",
+    #    },
+    #    {
+    #       'expression': 'raster1*raster2*raster3*(raster4>0)+(raster4<1)*-9999',
+    #       'symbol_to_path_map': { 
+    #           'raster1': "monfreda_2008_yield_poll_dep_ppl_fed_5min.tif", #https://storage.googleapis.com/critical-natural-capital-ecoshards/monfreda_2008_yield_poll_dep_ppl_fed_5min.tif
+    #           'raster2': r"workspace_poll_suff\churn\poll_suff_hab_ag_coverage_rasters\poll_suff_ag_coverage_prop_10s_Sc2v4_Griscom_CookPatton2035_smithpnv_md5_ffde2403583e30d7df4d16a0687d71fe.tif",
+    #           'raster3': "esa_pixel_area_ha_md5_1dd3298a7c4d25c891a11e01868b5db6.tif", # https://storage.googleapis.com/ecoshard-root/esa_pixel_area_ha_md5_1dd3298a7c4d25c891a11e01868b5db6.tif
+    #           'raster4': r"workspace_poll_suff\churn\ag_mask\Sc2v4_Griscom_CookPatton2035_smithpnv_md5_ffde2403583e30d7df4d16a0687d71fe_ag_mask.tif"
+    #        },
+    #        'target_nodata': -9999,
+    #        'target_pixel_size': (0.0027777777777777778,-0.0027777777777777778),
+    #        'resample_method': 'near',
+    #        'target_raster_path': "pollination_ppl_fed_on_ag_10s_Sc2v4_Griscom2035.tif",
+    #    },
+    #    {
+    #       'expression': 'raster1*raster2*raster3*(raster4>0)+(raster4<1)*-9999',
+    #       'symbol_to_path_map': {
+    #           'raster1': "monfreda_2008_yield_poll_dep_ppl_fed_5min.tif", #https://storage.googleapis.com/critical-natural-capital-ecoshards/monfreda_2008_yield_poll_dep_ppl_fed_5min.tif
+    #           'raster2': r"D:\repositories\pollination_sufficiency\workspace_poll_suff\churn\poll_suff_hab_ag_coverage_rasters\poll_suff_ag_coverage_prop_10s_Sc3v1_PNVnoag_md5_c07865b995f9ab2236b8df0378f9206f.tif",
+    #           'raster3': "esa_pixel_area_ha_md5_1dd3298a7c4d25c891a11e01868b5db6.tif", # https://storage.googleapis.com/ecoshard-root/esa_pixel_area_ha_md5_1dd3298a7c4d25c891a11e01868b5db6.tif
+    #           'raster4': r"D:\repositories\pollination_sufficiency\workspace_poll_suff\churn\ag_mask\Sc3v1_PNVnoag_md5_c07865b995f9ab2236b8df0378f9206f_ag_mask.tif",
+    #        },
+    #        'target_nodata': -9999,
+    #        'target_pixel_size': (0.0027777777777777778,-0.0027777777777777778),
+    #        'resample_method': 'near',
+    #        'target_raster_path': "pollination_ppl_fed_on_ag_10s_Sc3v1_PNVnoag.tif",
+    #    },
+    #]
 
     for calculation in calculation_list:
         raster_calculations_core.evaluate_calculation(
@@ -86,9 +145,24 @@ def main():
 
     return
 
-    #then back to docker with both those layers:
+    
+    # Have to use this for differencing because there are missing pixels in restoration (where ag got converted back)
+    #python add_sub_missing_as_0.py --subtract "D:\ecoshard\CI_PPC\pollination_ppl_fed_on_ag_10s_Sc1Renato0_5_md5_ccdae4.tif" "D:\ecoshard\CI_PPC\pollination_ppl_fed_on_ag_10s_esa2020_md5_0cf902.tif"
+    #python add_sub_missing_as_0.py --subtract "D:\ecoshard\CI_PPC\pollination_ppl_fed_on_ag_10s_Sc2v3_Griscom2050_md5_a86e5f.tif" "D:\ecoshard\CI_PPC\pollination_ppl_fed_on_ag_10s_esa2020_md5_0cf902.tif"
+    #python add_sub_missing_as_0.py --subtract "D:\ecoshard\CI_PPC\pollination_ppl_fed_on_ag_10s_Sc1v2_md5_28cb0.tif" "D:\ecoshard\CI_PPC\pollination_ppl_fed_on_ag_10s_esa2020_md5_0cf902.tif"
+    #python add_sub_missing_as_0.py --subtract "D:\ecoshard\CI_PPC\pollination_ppl_fed_on_ag_10s_Sc2v4_Griscom2035_md5_ffee3.tif" "D:\ecoshard\CI_PPC\pollination_ppl_fed_on_ag_10s_esa2020_md5_0cf902.tif"
+    #python add_sub_missing_as_0.py --subtract "D:\ecoshard\CI_PPC\pollination_ppl_fed_on_ag_10s_Sc3v1_PNVnoag.tif" "D:\ecoshard\CI_PPC\pollination_ppl_fed_on_ag_10s_esa2020_md5_0cf902.tif"
+
+    #then run realized pollination:
     # docker run -d --name pollination_container --rm -v %CD%:/usr/local/workspace therealspring/inspring:latest realized_pollination.py pollination_ppl_fed_on_ag_10s_esa2020mVCF.tif && docker logs pollination_container -f
     # docker run -d --name pollination_container --rm -v %CD%:/usr/local/workspace therealspring/inspring:latest realized_pollination.py pollination_ppl_fed_on_ag_10s_Sc2_Griscom_CookPatton.tif && docker logs pollination_container -f
+
+    #python realized_pollination.py --ppl_fed_path "pollination_ppl_fed_on_ag_10s_Sc3v1_PNVnoag   -  pollination_ppl_fed_on_ag_10s_esa2020_md5_0cf902.tif" --hab_mask_path "D:\repositories\pollination_sufficiency\workspace_poll_suff\churn\hab_mask\Sc3v1_PNVnoag_md5_c07865b995f9ab2236b8df0378f9206f_hab_mask.tif"
+    #python realized_pollination.py --ppl_fed_path "pollination_ppl_fed_on_ag_10s_Sc1v2_md5_28cb0   -  pollination_ppl_fed_on_ag_10s_esa2020_md5_0cf902.tif" --hab_mask_path "restoration_pnv0.0001_on_ESA2020_v2_md5_47613f8e4d340c92b2c481cc8080cc9d_hab_mask.tif" 
+    #python realized_pollination.py --ppl_fed_path "pollination_ppl_fed_on_ag_10s_Sc1Renato0_5_md5_ccdae4-esa2020_md5_0cf902_fixed.tif" --hab_mask_path "D:\repositories\pollination_sufficiency\Sc1Renato0_5_hab_mask_md5_6c555d.tif"
+    #python realized_pollination.py --ppl_fed_path "pollination_ppl_fed_on_ag_10s_Sc2v3_Griscom2050_md5_a86e5f   -  pollination_ppl_fed_on_ag_10s_esa2020_md5_0cf902.tif" --hab_mask_path "Sc2v3_Griscom_CookPatton2050_smithpnv_md5_82c2f863d49f5a25c0b857865bfdb4b0_hab_mask.tif"
+    #python realized_pollination.py --ppl_fed_path "pollination_ppl_fed_on_ag_10s_Sc2v4_Griscom2035_md5_ffee3   -  pollination_ppl_fed_on_ag_10s_esa2020_md5_0cf902.tif" --hab_mask_path "Sc2v4_Griscom_CookPatton2035_smithpnv_md5_ffde2403583e30d7df4d16a0687d71fe_hab_mask.tif" --suffix _Sc2v4Griscom2035-ESA
+
 
 
 if __name__ == '__main__':
